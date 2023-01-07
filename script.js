3a32c8d5780d1ba2326c2699b4abfcaf0d3f8f74
Galih Adhi Kusuma <noxturne.production@gmail.com>
2023-01-07 09:19:32 +0700

    pushed

--- script.js
+++ script.js
@@ -26,73 +26,114 @@ function validateForm() {
     valid = true
 }
 
-let treshold;
-function debounceFunction(timeout) {
-    clearTimeout(treshold)
-    treshold = setTimeout(() => {
-        validateForm()
-    }, timeout)
+const user = [
+    {
+        name: 'galih',
+        password: '123'
+    },
+    {
+        name: 'bima',
+        password: '321'
+    }
+]
+
+const inputName = document.getElementById('name')
+const inputPassword = document.getElementById('password')
+
+// login function
+function login() {
+    // get input user
+    const name = inputName.value
+    const password = inputPassword.value
+
+    // validate
+    
+    console.log(name, password)
+    // API (Application Programming Interface)
+
 }
 
-function getButtonWrapperBoundaries() {
-    const buttonWrapper = doc.querySelector('.button-wrapper')
-    const rect = buttonWrapper.getBoundingClientRect()
-
-    return {
-        x: rect.x, y: rect.y, w: rect.width
-    }
-}
-
-function moveButton() {
-    const currStyle = getComputedStyle(btn)
-    const currentLeft = currStyle.left
-    const direction = {
-        left: `${parseInt(currentLeft) + 50}px`,
-        right: `${parseInt(currentLeft) - 50}px`
-    }
-
-    const boundaries = getButtonWrapperBoundaries()
-    const rect = btn.getBoundingClientRect()
-
-
-    if (mousePost.x - rect.x <= rect.width / 2) {
-        btn.innerText = "ea gabisa 🤣"
-        if (rect.x >= boundaries.x + boundaries.w - 90) {
-            btn.style.left = direction.right
-            return
-        } else {
-            btn.style.left = direction.left
-            return
-        }
-    } else {
-        btn.innerText = "ea gabisa 🤣"
-        if (rect.x <= boundaries.x + 50) {
-            btn.style.left = direction.left
-            return
-        } else {
-            btn.style.left = direction.right
-            return
-        }
-    }
-}
-
-
-btn.addEventListener('mouseenter', () => {
-    if (valid) return
-
-    moveButton()
+inputName.addEventListener('keydown', (e) => {
+    login()
 })
 
-username.addEventListener('keydown', () => {
-    debounceFunction(300)
-})
-password.addEventListener('keydown', () => {
-    debounceFunction(300)
+inputPassword.addEventListener('keydown', () => {
+    login()
 })
 
-window.addEventListener('mousemove', (e) => {
-    mousePost.x = e.clientX
-    mousePost.y = e.clientY
-})
+
+
+
+
+
+
+// let treshold;
+// function debounceFunction(timeout) {
+//     clearTimeout(treshold)
+//     treshold = setTimeout(() => {
+//         validateForm()
+//     }, timeout)
+// }
+
+// function getButtonWrapperBoundaries() {
+//     const buttonWrapper = doc.querySelector('.button-wrapper')
+//     const rect = buttonWrapper.getBoundingClientRect()
+
+//     return {
+//         x: rect.x, y: rect.y, w: rect.width
+//     }
+// }
+
+// function moveButton() {
+//     const currStyle = getComputedStyle(btn)
+//     const currentLeft = currStyle.left
+//     const direction = {
+//         left: `${parseInt(currentLeft) + 50}px`,
+//         right: `${parseInt(currentLeft) - 50}px`
+//     }
+
+//     const boundaries = getButtonWrapperBoundaries()
+//     const rect = btn.getBoundingClientRect()
+
+
+//     if (mousePost.x - rect.x <= rect.width / 2) {
+//         btn.innerText = "ea gabisa 🤣"
+//         if (rect.x >= boundaries.x + boundaries.w - 90) {
+//             btn.style.left = direction.right
+//             return
+//         } else {
+//             btn.style.left = direction.left
+//             return
+//         }
+//     } else {
+//         btn.innerText = "ea gabisa 🤣"
+//         if (rect.x <= boundaries.x + 50) {
+//             btn.style.left = direction.left
+//             return
+//         } else {
+//             btn.style.left = direction.right
+//             return
+//         }
+//     }
+// }
+
+
+// btn.addEventListener('mouseenter', () => {
+//     if (valid) return
+
+//     moveButton()
+// })
+
+// username.addEventListener('keydown', () => {
+//     debounceFunction(300)
+// })
+// password.addEventListener('keydown', () => {
+//     debounceFunction(300)
+// })
+
+// window.addEventListener('mousemove', (e) => {
+//     mousePost.x = e.clientX
+//     mousePost.y = e.clientY
+// })
 
 
